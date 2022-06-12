1e8703679fa2376d1d664d6f8c61a94c63dca2a5
nandhini2109 <nandinich2109@gmail.com>
2022-06-12 16:53:56 +0530

    -This is My new commit - Here i started writing the c-This is My new commit - Here i started writing the cod-This is My new commit - Here i started writing the cod-This is My new commit - Here i started writing the cod-This is My new commit - Here i started writing the codeeeeode

---
+++ testing.py
@@ -0,0 +1,10 @@
+from package import calculator
+
+cal = calculator.calci(
+    a=30,
+    b=40
+)
+
+print(f"this is added value {cal.addition()}")
+
+print(f"this is subtracted value {cal.subtraction()}")
